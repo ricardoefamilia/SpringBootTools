862566d8ecc4433f90b80561417d02bbeade0eb2
ricardo <ricardoefamilia@gmail.com>
2022-02-09 22:10:39 -0300

    SpringBootTools 09/02/2020 - 22h10.

---
+++ src/test/java/springbootricardo01/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootricardo01.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
