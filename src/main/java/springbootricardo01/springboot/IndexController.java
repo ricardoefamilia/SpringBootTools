0af2c88b2eef7ab70374556f191f8fe5880741d5
ricardo <ricardoefamilia@gmail.com>
2022-02-09 22:23:43 -0300

    index.html e javax.xml.bind no pom.xml - 09/02/22 - 22h20.

---
+++ src/main/java/springbootricardo01/springboot/IndexController.java
@@ -0,0 +1,13 @@
+package springbootricardo01.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+}
