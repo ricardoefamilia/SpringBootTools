7d6f190f55b48650c1262b78cf28c9f15dabb6c2
ricardo <ricardoefamilia@gmail.com>
2022-02-11 23:38:18 -0300

    Listar e editar pessoas - 11/02/22 - 23h38.

--- src/main/java/springbootricardo01/controller/PessoaController.java
+++ src/main/java/springbootricardo01/controller/PessoaController.java
@@ -1,9 +1,14 @@
 package springbootricardo01.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import springbootricardo01.model.Pessoa;
 import springbootricardo01.repository.IPessoaRepository;
@@ -15,13 +20,41 @@ public class PessoaController {
 	private IPessoaRepository pessoaRepository;
 
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
-	public String inicio() {
-		return "cadastro/cadastropessoa";
+	public ModelAndView inicio() {
+		return pessoas();
+		/*
+		 * ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		 * andView.addObject("pessoaobj", new Pessoa()); return andView;
+		 */
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
-	public String salvar(Pessoa pessoa) {
+	@RequestMapping(method = RequestMethod.POST, value = "**/salvarpessoa")
+	public ModelAndView salvar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
-		return "cadastro/cadastropessoa";
+		return pessoas();
+		/*
+		 * ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		 * Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		 * andView.addObject("pessoas",pessoasIt); andView.addObject("pessoaobj", new
+		 * Pessoa()); return andView;
+		 */
 	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/listapessoas")
+	public ModelAndView pessoas() {
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		andView.addObject("pessoas",pessoasIt);
+		andView.addObject("pessoaobj", new Pessoa());
+		return andView;
+	}
+	
+	@GetMapping("/editarpessoa/{idpessoa}")
+	public ModelAndView editar(@PathVariable("idpessoa") Long idpessoa) {		
+		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		andView.addObject("pessoaobj", pessoa.get());
+		return andView;
+	}
+		
 }
